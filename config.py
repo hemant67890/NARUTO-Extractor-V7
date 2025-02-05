fdbfec411cc942fb6ced0b0a6d5698928596c9f9
krishan041 <krishandara04@gmail.com>
2025-02-05 20:01:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "8038725964:AAEkw01YH9JJ_soMDKAyqNSQz9q3JhX_yt0"
+    BOT_TOKEN = ""
     API_ID = 27536109
     API_HASH = "b84d7d4dfa33904d36b85e1ead16bd63"
     ADMIN = '6428531614'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://krishandara04:iLfRpsD0SbLVOBpP@cluster0.t88ua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mo:iLfRpsD0S0.t88ua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = "WARRIOR_EXTRACTOR"
     TXT_LOG = -1002447298131
     AUTH_LOG = -1002447298131
